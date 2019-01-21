deef34a1c6c2e2135b7fc48cc0de79c1d051d238
JL <lab.galom@outlook.com>
2019-01-22 00:54:49 +0200

    React alkeet

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,30 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = (course) => {
+    
+
+    return (
+        <div>
+            <p>
+            {course.name}
+            </p>
+        </div>
+        
+    )
+}
+const Content = (palikka) => {
+    
+    return (
+        <div>
+            <Part a = {'Reactin perusteet'} b = {10} />
+            <Part a = {'Tiedonvälitys propseilla'} b = {7} />
+            <Part a = {'Komponenttien tila'} b = {14} />
+        </div>
+    )
+}
+const Part = (osa) => {
+    
+    return (
+        <div>
+            <p>
+                {osa.a}, {osa.b}
+            </p>
+        </div>
+    )
+}
+const Total = (totali) => {
+    
+    return (
+        <div>
+            <p>
+                yhteensä {totali.summa} tehtävää
+            </p>
+        </div>
+    )
+}
+
 const App = () => {
     const course = 'Half Stack -sovelluskehitys'
-    const part1 = 'Reactin perusteet'
+    // const part1 = 'Reactin perusteet'
     const exercises1 = 10
-    const part2 = 'Tiedonvälitys propseilla'
+    // const part2 = 'Tiedonvälitys propseilla'
     const exercises2 = 7
-    const part3 = 'Komponenttien tila'
+    // const part3 = 'Komponenttien tila'
     const exercises3 = 14
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-            {part2} {exercises2}
-            </p>
-            <p>
-            {part3} {exercises3}
-            </p>
-            <p>
-                <p>yhteensä {exercises1 + exercises2 + exercises3} tehtävää</p>
-            </p>
+            <Header name={course}/>
+            <Content />
+            <Total summa={exercises1 + exercises2 + exercises3}/>
         </div>
     )
 }
